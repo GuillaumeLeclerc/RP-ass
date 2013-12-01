294ad99b6ba5404c2fda049da43dde11e88b45ce
Guillaume Leclerc <guillaume.leclerc.work@gmail.com>
2013-12-01 23:25:58 +0100

    Partie 1 (tests Ok)

--- src/main/scala/suggestions/gui/SwingApi.scala
+++ src/main/scala/suggestions/gui/SwingApi.scala
@@ -10,6 +10,7 @@ import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
 import rx.lang.scala.Observable
+import rx.lang.scala.subscriptions.Subscription
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,7 +52,19 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
+    def textValues: Observable[String] = {
+      Observable.apply(obs => {
+    	  val myReaction : Reaction= {
+	        case ValueChanged(x) => obs.onNext(x.text)
+	        case _ =>
+	      }
+    	  field.subscribe(myReaction)
+    	  Subscription{
+    	    field.unsubscribe(myReaction)
+    	  }
+      })
+      
+    }
 
   }
 
@@ -62,7 +75,18 @@ trait SwingApi {
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = ???
+    def clicks: Observable[Button] = {
+      Observable.apply(obs => {
+        val myReaction : Reaction = {
+          case ButtonClicked(x) => obs.onNext(x)
+          case _ =>
+        }
+        button.subscribe(myReaction)
+        Subscription{
+          button.unsubscribe(myReaction)
+        }
+      })
+    }
 
   }
 
